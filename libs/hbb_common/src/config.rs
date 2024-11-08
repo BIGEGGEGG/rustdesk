24adde69d5e898a62a7d100df6b7108086332268
BIGEGGEGG <100254784+BIGEGGEGG@users.noreply.github.com>
2024-11-08 13:46:38 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.bingjie.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.bingjie.top"];
 pub const PUBLIC_RS_PUB_KEY: &str = "qbj15345316953";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
